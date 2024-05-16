56fdaba2a587403b42f1c7d135e47fa275fb959b
kc <toilaluan@github.com>
2024-05-16 17:44:03 +0700

    update script

--- unet/TonThuongWithPretrained.sh
+++ unet/TonThuongWithPretrained.sh
@@ -3,3 +3,9 @@ python test.py --type_damaged ung_thu_thuc_quan_20230620 --type_pretrained im1k 
 python test.py --type_damaged viem_da_day_20230620 --type_pretrained im1k --root_path "/mnt/tuyenld/data/endoscopy/" --type_seg "TonThuong"
 python test.py --type_damaged viem_thuc_quan_20230620 --type_pretrained im1k --root_path "/mnt/tuyenld/data/endoscopy/" --type_seg "TonThuong"
 python test.py --type_damaged viem_loet_hoanh_ta_trang_20230620 --type_pretrained im1k --root_path "/mnt/tuyenld/data/endoscopy/" --type_seg "TonThuong"
+
+python test.py --type_damaged ung_thu_da_day_20230620 --type_pretrained endoscopy --root_path "/mnt/tuyenld/data/endoscopy/" --type_seg "TonThuong"
+python test.py --type_damaged ung_thu_thuc_quan_20230620 --type_pretrained endoscopy --root_path "/mnt/tuyenld/data/endoscopy/" --type_seg "TonThuong"
+python test.py --type_damaged viem_da_day_20230620 --type_pretrained endoscopy --root_path "/mnt/tuyenld/data/endoscopy/" --type_seg "TonThuong"
+python test.py --type_damaged viem_thuc_quan_20230620 --type_pretrained endoscopy --root_path "/mnt/tuyenld/data/endoscopy/" --type_seg "TonThuong"
+python test.py --type_damaged viem_loet_hoanh_ta_trang_20230620 --type_pretrained endoscopy --root_path "/mnt/tuyenld/data/endoscopy/" --type_seg "TonThuong"
